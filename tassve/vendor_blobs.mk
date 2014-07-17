8cdfefe970a077183f8e8f394c1c7e7735ecd2c1
Marco Milanese <marcomilanese7@gmail.com>
2014-07-17 13:44:26 +0200

    Revert "Samsung vendor cleanup"
    This reverts commit b4da6df315f53f1c9f54d79bc73b6e47c2c43813.
    
    Conflicts:
    
    	cooperve/vendor_blobs.mk
    	tassve/vendor_blobs.mk
    	totoro/vendor_blobs.mk
    
    Change-Id: I696aff254e2187bd1c0bc68e7b78ffcf4afb0250

--- tassve/vendor_blobs.mk
+++ tassve/vendor_blobs.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
+
 # Camera
 PRODUCT_COPY_FILES += \
     vendor/samsung/tassve/proprietary/lib/libcamera.so:system/lib/libcamera.so
